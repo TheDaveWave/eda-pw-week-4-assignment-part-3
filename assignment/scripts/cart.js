cfade2fe1de66ca199c6f7f8961e8baebff7fc12
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-09 12:55:50 -0500

    created an array called basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// - Create a global variable named `basket` and set it to an empty array.
+let basket = [];
+
